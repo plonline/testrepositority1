c88bc21d4ee9ff417b141437347d9a1594884d8c
Lakshmi P <plonlinepro@gmail.com>
2016-01-29 20:16:56 -0500

    First Commit 1

---
+++ src/mydemo/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package mydemo;
+
+/**
+ * @author Raghava
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		System.out.println("Hello World!");
+		
+	
+	}
+
+}
